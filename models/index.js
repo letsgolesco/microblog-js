a8aa6db0adbf0f9f22eca02a7f8b5903ceba1f0b
Richard Lesco <richardlesco@gmail.com>
2014-08-26 16:53:09 -0400

    Mongoose models file

---
+++ models/index.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var MicroblogSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true,
+    match: /^([\w ,.!?]{1,100})$/
+  }
+});
+
+var EntrySchema = new mongoose.Schema({
+  text: {
+    required: true,
+    type: String,
+    trim: true,
+    match: /^([\w ,.!?]{1,160})$/
+  },
+
+  image: {
+    type: String,
+    required: false
+  },
+
+  microblog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Microblog'
+  }
+});
+
+module.exports = {
+  Microblog: mongoose.model('Microblog', MicroblogSchema),
+  Entry: mongoose.model('Entry', EntrySchema)
+};
