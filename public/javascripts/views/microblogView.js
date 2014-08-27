9a2778b440955a5fa38d6e844cd0275f34f093b9
Richard Lesco <richardlesco@gmail.com>
2014-08-27 17:49:30 -0400

    backbone creates entries now!

--- public/javascripts/views/microblogView.js
+++ public/javascripts/views/microblogView.js
@@ -6,15 +6,39 @@ define([
     'backbone',
     'handlebars',
     'views/entryCollectionView',
-    'collections/entryCollection'
-], function ($, _, Backbone, Handlebars, EntryCollectionView, EntryCollection) {
+], function ($, _, Backbone, Handlebars, EntryCollectionView) {
 
     var MicroblogView = Backbone.View.extend({
 
         className: 'microblog-container',
 
+        events: {
+            'click .post-entry': 'postEntry'
+        },
+
+        postEntry: function(e) {
+            var that = this;
+            this.model.entries.create({
+                text: $('.entry-text').val(),
+                image: $('.entry-image').val(),
+                microblog: this.model.id
+            }, {
+                success: function() {
+                    that.render();
+                    console.log('POST WORKED');
+                },
+                error: function() {
+                    console.log('POST ERROR');
+                }
+            });
+        },
+
         render: function () {
             this.$el.html('');
+            var template = $('#blogDetailTemplate').html();
+            var compiled = Handlebars.compile(template);
+            var html = compiled(this.model.attributes);
+            this.$el.append(html);
             var entryCollectionView = new EntryCollectionView({ collection: this.model.entries });
             this.$el.append(entryCollectionView.render().el);
             return this;
